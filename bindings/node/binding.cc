9f9cf903b9cc05706696fa3a054905796f81a470
houcheng <houcheng@gmail.com>
2024-11-26 18:59:10 +0800

    Changes folder structures

--- bindings/node/binding.cc
+++ bindings/node/binding.cc
@@ -10,22 +10,10 @@ const napi_type_tag LANGUAGE_TYPE_TAG = {0x8AF2E5212AD58ABF,
                                          0xD5006CAD83ABBA16};
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-  auto fsharp_exports = Napi::Object::New(env);
-  fsharp_exports["name"] = Napi::String::New(env, "fsharp");
-  auto fsharp_language =
-      Napi::External<TSLanguage>::New(env, tree_sitter_fsharp());
+  exports["name"] = Napi::String::New(env, "fsharp");
+  auto fsharp_language = Napi::External<TSLanguage>::New(env, tree_sitter_fsharp());
   fsharp_language.TypeTag(&LANGUAGE_TYPE_TAG);
-  fsharp_exports["language"] = fsharp_language;
-  exports["fsharp"] = fsharp_exports;
-
-  auto signature_exports = Napi::Object::New(env);
-  signature_exports["name"] = Napi::String::New(env, "fsharp_signature");
-  auto signature_language =
-      Napi::External<TSLanguage>::New(env, tree_sitter_fsharp_signature());
-  signature_language.TypeTag(&LANGUAGE_TYPE_TAG);
-  signature_exports["language"] = signature_language;
-  exports["signature"] = signature_exports;
-
+  exports["language"] = fsharp_language;
   return exports;
 }
 
